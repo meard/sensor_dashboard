cdebf69f3c0253ac275e5e6e4d5fe836128c745c
Navneet0645 <2022HT56073@wilp.bits-pilani.ac.in>
2024-05-18 19:31:29 +0530

    #35.1 test_bash 0.1

--- Input_Device/test_bash.sh
+++ Input_Device/test_bash.sh
@@ -3,7 +3,7 @@
 
 cd ~/navneet_ws/2022HT56073_SEM4/sensor_dashboard/dashboard && sleep 2 && node ./node_modules/gulp/bin/gulp.js && sleep 20 &
 
-cd ~/navneet_ws/2022HT56073_SEMs4/sensor_dashboard/Input_Device && sleep 2 && python3 Input_Device.py && sleep 20
+cd ~/navneet_ws/2022HT56073_SEM4/sensor_dashboard/Input_Device && sleep 2 && python3 Input_Device.py && sleep 20
 
 # D- ~/navneet_ws/2022HT56073_SEM4/sensor_dashboard/dashboard 
 # CODE: node ./node_modules/gulp/bin/gulp.js
